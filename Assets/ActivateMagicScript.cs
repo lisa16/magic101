3828a61f56278b7849988a869b132cce18e0924a
Eric Kim <bosung90@gmail.com>
2015-01-24 14:45:35 -0800

    adding particle effect for tornado, meteorite, holy blast, lightening with activation using keys q,w,e,r

---
+++ Assets/ActivateMagicScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ActivateMagicScript : MonoBehaviour {
+
+	public ParticleSystem lightening, meteorite, tornado, holyblast;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKey(KeyCode.Q))
+		{
+			lightening.Play();
+		}
+		if(Input.GetKey(KeyCode.W))
+		{
+			meteorite.Play();
+		}
+		if(Input.GetKey(KeyCode.E))
+		{
+			tornado.Play();
+		}
+		if(Input.GetKey(KeyCode.R))
+		{
+			holyblast.Play();
+		}
+	}
+}
