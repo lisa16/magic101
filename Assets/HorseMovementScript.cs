6ecbfe66ea73781f0a9b0b3d949977b2ae1cd201
Eric Kim <bosung90@gmail.com>
2015-01-24 17:08:34 -0800

    added horse move to random direction with rotation toward the direction.

---
+++ Assets/HorseMovementScript.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorseMovementScript : MonoBehaviour {
+
+	float changeDirectionTicker = 0;
+	Animator animator;
+
+	Vector3 speed = Vector3.zero;
+
+	// Use this for initialization
+	void Start () {
+		this.rigidbody.velocity = Vector3.zero;
+		animator = GetComponent<Animator> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.velocity = speed;
+		changeDirectionTicker += Time.deltaTime;
+		animator.SetFloat ("Speed", rigidbody.velocity.magnitude);
+		if(changeDirectionTicker >=5)
+		{
+			changeDirectionTicker = 0;
+			speed = new Vector3(Random.Range(-10, 10), 0, Random.Range(-10, 10));
+		}
+
+		RotateTowardVelocity ();
+	}
+
+	void RotateTowardVelocity()
+	{
+		transform.rotation = Quaternion.LookRotation(rigidbody.velocity);
+	}
+}
